11879aa76d73eeb1a9db08cc6d960986730b378a
ahmedmohamed13896 <ahmedmohamed77765@hotmail.com>
2024-09-04 04:00:59 +0300

    add coming soon page

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+const items = document.querySelectorAll('.animated-content .item');
+let currentIndex = 1;
+
+setInterval(() => {
+    // Remove 'active' class from all items
+    items.forEach(item => item.classList.remove('active'));
+
+    // Add 'active' class to the current item
+    items[currentIndex].classList.add('active');
+
+    // Update the index to the next item
+    currentIndex = (currentIndex + 1) % items.length;
+}, 5000); // Change the class every 1 second (1000 ms)
