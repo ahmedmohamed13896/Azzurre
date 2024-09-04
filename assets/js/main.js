f8080035d2ffd9bd217b067342e085b45db89f40
ahmedmohamed13896 <ahmedmohamed77765@hotmail.com>
2024-09-04 06:13:05 +0300

    add gif img to support ios

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,14 @@ setInterval(() => {
     // Update the index to the next item
     currentIndex = (currentIndex + 1) % items.length;
 }, 5000); // Change the class every 1 second (1000 ms)
+
+
+// Check if the browser supports WebM format
+var video = document.createElement('video');
+var canPlayWebM = video.canPlayType('video/webm; codecs="vp8, vorbis"');
+
+if (canPlayWebM === '') {
+    // Browser cannot play WebM, hide video and show GIF
+    document.getElementById('background-video').style.display = 'none';
+    document.getElementById('background-gif').style.display = 'block';
+}
