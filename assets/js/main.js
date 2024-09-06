e0875661c6545d4cec1ef091dfe8ae1aea4c19a5
ahmedmohamed13896 <ahmedmohamed77765@hotmail.com>
2024-09-06 20:00:37 +0300

    improve animation using requestAnimationFrame

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,42 +14,52 @@ setInterval(() => {
 }, 5000); // Change the class every 1 second (1000 ms)
 
 
+let currentImageIndex = 0; // Index for the current image
+const startIndex = 2000;
+const endIndex = 2165;
+const imagePath = './assets/images/sequence-images-min/azzurre-img';
+const imageExtension = '-min.png';
 
-// Function to preload all secquenceImages
+// Preload all images
 function preloadImages(callback) {
-    let currentIndex = 2000;
-    let lastItemNumber = 2164;
-
-    for (let i = 2000; i <= lastItemNumber; i++) {
+    let loadedImages = 0;
+    for (let i = startIndex; i < endIndex; i++) {
         let img = new Image();
-        img.src = './assets/images/sequence-images-min/azzurre-img' + i + '-min.png';
-        secquenceImages.push(img);
-
+        img.src = imagePath + i + imageExtension;
         img.onload = function () {
-            currentIndex++;
-            if (currentIndex === lastItemNumber) {
-                callback();
+            secquenceImages[i - startIndex] = img;
+            loadedImages++;
+            if (loadedImages === (endIndex - startIndex)) {
+                callback(); // All images loaded
                 document.querySelector('.loader_section').style.display =  'none';
             }
+        };
+        img.onerror = function () {
+            console.error('Failed to load image: ' + img.src);
         };
     }
 }
 
-function startAnimation(){
-    let startIndex = 2000;
-    const endIndex = 2165;
-    setInterval(function () {
-        document.querySelector('#png-sequence img').src = './assets/images/sequence-images-min/azzurre-img' + startIndex + '-min.png';
-        startIndex++;
-        startIndex = startIndex === endIndex ? 2000 : (startIndex);
-    }, 30);
+// Function to animate images using requestAnimationFrame
+function animateFrames(timestamp) {
+    if (!window.lastFrameTime) {
+        window.lastFrameTime = timestamp;
+    }
+    const elapsed = timestamp - window.lastFrameTime;
+    const frameDelay = 30; // 30ms per frame
+    if (elapsed > frameDelay) {
+        document.querySelector('#png-sequence img').src = secquenceImages[currentImageIndex].src;
+        currentImageIndex = (currentImageIndex + 1) % secquenceImages.length; // Loop through images
+        window.lastFrameTime = timestamp;
+    }
+    requestAnimationFrame(animateFrames); // Continue animation
 }
 
-// Preload images and start the animation after loading
+// Start animation after preloading images
+function startAnimation() {
+    requestAnimationFrame(animateFrames);
+}
+
 window.onload = function () {
-    preloadImages(startAnimation)    
+    preloadImages(startAnimation);
 };
-
-
-
-
