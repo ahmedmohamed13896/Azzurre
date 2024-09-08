06964ea7bb9fc6891f5cef398364affaada9d371
ahmedmohamed13896 <ahmedmohamed77765@hotmail.com>
2024-09-08 17:08:24 +0300

    replace video and remove all images

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,46 +14,6 @@ setInterval(() => {
 }, 5000); // Change the class every 1 second (1000 ms)
 
 
-let currentImageIndex = 0; // Index for the current image
-const startIndex = 2000;
-const endIndex = 2165;
-const imagePath = './assets/images/sequence-images-min/azzurre-img';
-const imageExtension = '-min.png';
-
-// Preload all images
-function preloadImages(callback) {
-    let loadedImages = 0;
-    for (let i = startIndex; i < endIndex; i++) {
-        let img = new Image();
-        img.src = imagePath + i + imageExtension;
-        img.onload = function () {
-            secquenceImages[i - startIndex] = img;
-            loadedImages++;
-            if (loadedImages === (endIndex - startIndex)) {
-                callback(); // All images loaded
-                document.querySelector('.loader_section').style.display =  'none';
-            }
-        };
-        img.onerror = function () {
-            console.error('Failed to load image: ' + img.src);
-        };
-    }
-}
-
-// Function to animate images using requestAnimationFrame
-function animateFrames(timestamp) {
-    if (!window.lastFrameTime) {
-        window.lastFrameTime = timestamp;
-    }
-    const elapsed = timestamp - window.lastFrameTime;
-    const frameDelay = 30; // 30ms per frame
-    if (elapsed > frameDelay) {
-        document.querySelector('#png-sequence img').src = secquenceImages[currentImageIndex].src;
-        currentImageIndex = (currentImageIndex + 1) % secquenceImages.length; // Loop through images
-        window.lastFrameTime = timestamp;
-    }
-    requestAnimationFrame(animateFrames); // Continue animation
-}
 
 // Start animation after preloading images
 function startAnimation() {
@@ -61,5 +21,5 @@ function startAnimation() {
 }
 
 window.onload = function () {
-    preloadImages(startAnimation);
+    document.querySelector('.loader_section').style.display =  'none';
 };
