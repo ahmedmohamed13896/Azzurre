8e2dd5c7b8461f572bad805ce77d37722b1f689a
ahmedmohamed13896 <ahmedmohamed77765@hotmail.com>
2024-09-09 02:29:16 +0300

    fix mac bgcolor

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,4 +22,7 @@ function startAnimation() {
 
 window.onload = function () {
     document.querySelector('.loader_section').style.display =  'none';
+    if (navigator.userAgent.indexOf('Mac OS X') !== -1) {
+        document.querySelector(".main-section").classList.add('mac');
+      }
 };
